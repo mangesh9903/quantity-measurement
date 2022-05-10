c7c94b1a8ffbd1cc73b32735428185b6eccc6da2
mangesh9903 <mangeshb9903@gmail.com>
2022-05-10 23:10:49 +0530

    ADDED: TC 1.3 Type Check.

--- src/test/java/com/bridgelabz/QuanityTest.java
+++ src/test/java/com/bridgelabz/QuanityTest.java
@@ -6,22 +6,38 @@ import org.junit.Test;
 public class QuanityTest {
 
     /**
-     *  TC 1.1 Given 0 Feet and 0 Feet Should Return equal
+     * TC 1.1 Given 0 Feet and 0 Feet Should Return equal
      */
     @Test
-    public void given0FeetAnd0Feet_shouldReturnEqual(){
+    public void given0FeetAnd0Feet_shouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
-        Assert.assertEquals(feet1,feet2);
+        Assert.assertEquals(feet1, feet2);
     }
 
     /**
-     *  TC 1.2 Null Check
+     * TC 1.2 Null Check
      */
     @Test
-    public void givenNullFeetValue_shouldReturnFalse(){
+    public void givenNullFeetValue_shouldReturnFalse() {
         Feet feet = new Feet(0.0);
-        Assert.assertNotEquals(null,feet);
+        Assert.assertNotEquals(null, feet);
 
+    }
+
+    /**
+     * TC 1.3 Type Check
+     */
+    @Test
+    public void givenReferenceObject_whenSame_shouldReturnTrue() {
+        Feet feet = new Feet(0.0);
+        Assert.assertSame(feet, feet);
+    }
+
+    @Test
+    public void givenReferenceObject_whenNotSame_shouldReturnTrue() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertNotSame(feet1, feet2);
     }
 }
