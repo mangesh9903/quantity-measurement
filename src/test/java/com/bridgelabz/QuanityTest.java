6ecada00f2690b6621d9ad4885cc663c188a1e98
mangesh9903 <mangeshb9903@gmail.com>
2022-05-10 19:48:11 +0530

    ADDED: TC 1.2 Null Check.

--- src/test/java/com/bridgelabz/QuanityTest.java
+++ src/test/java/com/bridgelabz/QuanityTest.java
@@ -5,10 +5,23 @@ import org.junit.Test;
 
 public class QuanityTest {
 
+    /**
+     *  TC 1.1 Given 0 Feet and 0 Feet Should Return equal
+     */
     @Test
     public void given0FeetAnd0Feet_shouldReturnEqual(){
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
+    }
+
+    /**
+     *  TC 1.2 Null Check
+     */
+    @Test
+    public void givenNullFeetValue_shouldReturnFalse(){
+        Feet feet = new Feet(0.0);
+        Assert.assertNotEquals(null,feet);
+
     }
 }
